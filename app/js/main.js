78ac2ceefad1abef064d60062905e345ec03abdb
Boyzie Mathis <boyzielee@gmail.com>
2015-10-20 11:04:04 -0400

    example 1

--- app/js/main.js
+++ app/js/main.js
@@ -15,7 +15,39 @@ var _moment = require('moment');
 
 var _moment2 = _interopRequireDefault(_moment);
 
-console.log('Hello, World');
+// Constructor (Blueprint)
+var Animal = function Animal(name, type, legs, tail, sound) {
+
+  this.name = name;
+  this.type = type;
+  this.legs = legs;
+  this.hasTail = tail;
+
+  // this is realted to the instace that was crated for animal.
+  // i.e. peanut
+  this.speak = function () {
+    return this.name + 'says:' + 'sound';
+  };
+};
+
+// Instance
+window.peanut = new Animal('Sparky', 'Dog', 4, true, 'bark');
+window.jaws = new Animal('Burce', 'Shark', 0, true, 'da na...');
+window.george = new Animal('Tyler', 'Monkey', 2, true, 'ooh ah aha ah');
+
+console.log(peanut.type);
+console.log(peanut.speak());
+
+// var x = {};
+// var y = new Object();
+// console.dir(x);
+// console.dir(y);
+
+// var a = [];
+// console.log(a);
+
+// var b = new Array();
+// console.
 
 },{"jquery":2,"moment":3,"underscore":4}],2:[function(require,module,exports){
 /*!
@@ -13854,7 +13886,7 @@ return jQuery;
     // Compile the template source, escaping string literals appropriately.
     var index = 0;
     var source = "__p+='";
-    text.replace(matcher, function(match, escape, interpolate, evaluate, offset) { 
+    text.replace(matcher, function(match, escape, interpolate, evaluate, offset) {
       source += text.slice(index, offset).replace(escaper, escapeChar);
       index = offset + match.length;
 
--- js/main.js
+++ js/main.js
@@ -2,4 +2,40 @@ import $ from 'jquery';
 import _ from 'underscore';
 import moment from 'moment';
 
-console.log('Hello, World');
+// Constructor (Blueprint)
+let Animal = function (name, type, legs, tail, sound) {
+
+  this.name = name;
+  this.type = type;
+  this.legs = legs;
+  this.hasTail = tail;
+
+// this is realted to the instace that was crated for animal.
+// i.e. peanut
+  this.speak = function() {
+    return this.name + 'says:' +'sound';
+  };
+
+};
+
+
+// Instance 
+window.peanut = new Animal('Sparky', 'Dog', 4, true, 'bark');
+window.jaws = new Animal('Burce', 'Shark', 0, true, 'da na...');
+window.george = new Animal('Tyler', 'Monkey', 2, true, 'ooh ah aha ah');
+
+
+
+console.log(peanut.type);
+console.log(peanut.speak());
+
+// var x = {};
+// var y = new Object();
+// console.dir(x);
+// console.dir(y);
+
+// var a = [];
+// console.log(a);
+
+// var b = new Array();
+// console.
